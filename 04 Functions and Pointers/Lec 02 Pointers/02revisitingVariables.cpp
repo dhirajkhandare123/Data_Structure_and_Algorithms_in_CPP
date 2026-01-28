781e49a426db666b7b5da470536d2eeefaf9a710
Dhiraj Khandare <dhirajkhandare76@gmail.com>
2026-01-28 22:38:30 +0530

    Add files via upload

---
+++ 04 Functions and Pointers/Lec 02 Pointers/02revisitingVariables.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int x = 5;
+    // cout<<x<<endl;
+    // cout<<&x<<endl;
+
+    // float x = 5;
+    // cout<<&x<<endl;
+
+    // char ch = 's';
+    // cout<<&ch<<endl;
+
+    int x=5;
+    int y=5;
+
+    cout<<&x<<endl;
+    cout<<&y<<endl;
+
+    return 0;
+}
