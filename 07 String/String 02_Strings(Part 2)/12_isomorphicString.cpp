38971303024892cf9216c1f89b783e950b5502b2
Dhiraj Khandare <dhirajkhandare76@gmail.com>
2026-03-25 20:43:52 +0530

    Add files via upload

---
+++ 07 String/String 02_Strings(Part 2)/12_isomorphicString.cpp
@@ -0,0 +1,4 @@
+// Given two string s and t, determine if they are isomorphic.
+
+// leetcode 205
+
---
+++ 07 String/String 02_Strings(Part 2)/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string s = "dhiraj";
+    int n = s.length();
+    int count=0;
+
+    for(int i=0;i<n;i++){
+        if(n==1) break;
+        if(n==2 && s[0]!=s[1]) count=1;
+        if(i==1){
+            if(s[i+1]!=s[i]){
+                count++;
+            }
+        }
+        else if(i==n-1){
+            if(s[i-1]!=s[i]){
+                count++;
+            }
+        }
+        else if(s[i]!=s[i-1] && s[i]!=s[i+1]){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
