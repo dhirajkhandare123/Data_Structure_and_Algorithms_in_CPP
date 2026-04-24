15650be93cb33e75a8ce41aac85721f50a68db12
Dhiraj Khandare <dhirajkhandare76@gmail.com>
2026-04-24 21:35:31 +0530

    Add files via upload

---
+++ 10 Searching/02 Searching_(Searching_Part_02)/01mountainArray.cpp
@@ -0,0 +1,25 @@
+// Leetcode 852
+#include<vector>
+class Solution {
+    public:
+        int peakIndexInMountainArray(vector<int>& arr) {
+            int n=arr.size();
+            int low=1;
+            int high=n-2;
+            int idx=-1;
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+                    idx=mid;
+                    break;
+                }
+                else if(arr[mid]>arr[mid+1]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            return idx;
+        }
+    };
